bbbcf996a5fcb0247c3339b809d2b90cc019b612
LeKieu <94425786+UIT-21522268@users.noreply.github.com>
2023-11-21 23:46:04 +0700

    fix active header

--- inc/footer.php
+++ inc/footer.php
@@ -42,6 +42,7 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script>
+    function setActive() {
     let navbar = document.getElementById('nav-bar');
     let a_tags =navbar.getElementsByTagName('a');
 
@@ -52,6 +53,7 @@
         if (document.location.href.indexOf(file_name) >= 0) {
             a_tags[i].classList.add('active');
         }
+    }
     }
     setActive();
 </script>
--- inc/header.php
+++ inc/header.php
@@ -7,7 +7,7 @@
     $contact_r = mysqli_fetch_assoc(select($contact_q, $values,'i'));
 ?>
 
-<nav id="navbar" class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
+<nav id="nav-bar" class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
     <div class="container-fluid">
         <!-- chi can nhap vao HRJ HOTEL -> den trang index.php
                 fw: font-weight
