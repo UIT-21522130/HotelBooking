67f62c7960c304fc843baaaf4fecbb83b4d2fe7d
UIT-21522130 <96097433+UIT-21522130@users.noreply.github.com>
2023-12-19 17:24:21 +0700

    booking_admin

---
+++ admin/new_bookings.php
@@ -0,0 +1,55 @@
+<?php
+    require('inc/essentials.php');
+    require('inc/db_config.php');
+    adminLogin();    
+?>
+
+<!DOCTYPE html>
+<html lang = "vi">  
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel - New bookings </title>
+    <?php require('inc/links.php'); ?>
+</head>
+<body class="bg-light">
+   
+    <?php require('inc/header.php'); ?>
+
+    <div class="container_fluid" id="main-content">
+        <div class="row">
+            <div class="col-lg-10 ms-auto p-4 overflow-hidden">
+                <h3 class="mb-4">New bookings</h3>
+                    
+                    <div class="card border-0 shadow-sm mb-4" >
+                            <div class="card-body">
+
+                            <div class="text-end mb-4">
+                                <input type="text" oninput = "search_user(this.value)" class="form-control shadow-none w-25 ms-auto" placeholder="Type to search...">
+                            </div>
+
+                            <div class="table-responsive">
+                                <table class="table table-hover border ">
+                                    <thead class="stickey-top">
+                                        <tr class="bg-dark text-light">
+                                            <th scope="col">#</th>
+                                            <th scope="col">User Details</th>
+                                            <th scope="col">Room Details</th>
+                                            <th scope="col">Booking Details</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="table-data"></tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>                
+            </div>
+        </div>
+    </div>
+
+    <?php require('inc/scripts.php');?>
+    <script src="scripts/new_bookings.js"></script>
+</body>
+</html>
